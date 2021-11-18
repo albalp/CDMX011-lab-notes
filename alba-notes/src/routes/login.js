333698b8ce774ce03577c7ba388dfb744b36146a
Alba <alba.lp95@gmail.com>
2021-11-17 23:50:22 -0600

    Creation of the buttons to link them to the respective routes

--- alba-notes/src/routes/login.js
+++ alba-notes/src/routes/login.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Login extends Component {
   render() {
     return (
+      
       <div className ='sectionMain'>
       <div className ='div-title'>
         <h1>NOTE MY NOTES</h1>
+        <Link to="/allNotes">
+        <button>LOGIN</button>
+        </Link>
+        <Link to = '/register'>
+        <p>¿No tienes cuenta? ¡Regístrate!</p>
+        </Link>
+        <Link to = '/allNotes'>
+        <p>Continúa con Google</p>
+        </Link>
       </div>
       </div>
+      
     );
   }
 }
